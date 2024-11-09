d3476b10ab9cd5967cfd7b39b16b4ddb94d496d0
OleynikAleksandr <_email@_github>
2024-11-09 13:00:33 +0100

    AssemblyInformationalVersion("1.0.2-Alpha")

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Номер сборки
 //      Редакция
 //
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
-[assembly: AssemblyInformationalVersion("1.0.1-Alpha")] // Информационная версия (может включать текст)
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyInformationalVersion("1.0.2-Alpha")] // Информационная версия (может включать текст)
